a02d2194a5f9a82f4658ecccbe850504b2d100bd
AIhmed <bouliche.ahmed.2@gmail.com>
2023-02-26 10:52:01 +0000

    structuring for multicloud infrastructure

--- aws_solution/main.tf
+++ aws_solution/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "Hashicorp/aws"
-      version = "3.27"
-    }
-  }
-}
-
-provider "aws" {
-  region     = "us-east-1"
-  access_key = var.access_key_id
-  secret_key = var.secret_key_id
-}
-
 module "networking" {
   source = "./modules/networking"
   vm1_id = module.compute.vm1_id
